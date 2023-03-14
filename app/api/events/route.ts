e642c327222b6636f8a39654581f44095d996656
Ahmed Mayo <ahmedsabdelaziz@gmail.com>
2023-03-15 00:37:10 +0200

    get events endpoint

---
+++ app/api/events/route.ts
@@ -0,0 +1,42 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import { getEvents } from "@/server/controllers/events";
+
+export async function GET(request: NextRequest) {
+  const authorizationHeader = request.headers.get("Authorization") || "";
+  const userId = authorizationHeader.split(" ")[1] || "";
+  if (!userId) {
+    return new NextResponse("Unauthorized access", { status: 401 });
+  }
+
+  const userLocation = request.ip || "";
+
+  const { searchParams } = request.nextUrl;
+  const page = Number(searchParams.get("page")) || 0;
+  const pageSize = Number(searchParams.get("pageSize")) || 10;
+  const search = searchParams.get("search") || undefined;
+  const filters = {
+    action_id: searchParams.get("action_id") || undefined,
+    actor_id: searchParams.get("actor_id") || undefined,
+    target_id: searchParams.get("target_id") || undefined,
+  };
+
+  try {
+    const result = await getEvents({
+      userId,
+      userLocation,
+      page,
+      pageSize,
+      search,
+      filters,
+    });
+
+    return NextResponse.json(result);
+  } catch (error) {
+    console.error("Search Events:", error);
+
+    return new NextResponse("Internal server error", {
+      status: 500,
+    });
+  }
+}
---
+++ server/controllers/events.ts
@@ -0,0 +1,140 @@
+import type { Event, Action, User, Prisma } from "@prisma/client";
+import prisma from "../prisma";
+
+interface GetEventsArgs {
+  userId: string;
+  userLocation: string;
+  page: number;
+  pageSize: number;
+  search?: string;
+  filters?: {
+    action_id?: string;
+    actor_id?: string;
+    target_id?: string;
+  };
+}
+interface GetEventsResult {
+  data: Array<
+    Pick<Event, "id" | "occurred_at"> & {
+      action: Pick<Action, "name">;
+      actor: Pick<User, "name">;
+    }
+  >;
+  count: number;
+}
+export async function getEvents({
+  userId,
+  userLocation,
+  page,
+  pageSize,
+  search,
+  filters,
+}: GetEventsArgs): Promise<GetEventsResult> {
+  const where: Prisma.EventAggregateArgs["where"] = {
+    OR: [
+      {
+        action: {
+          name: {
+            contains: search,
+            mode: "insensitive",
+          },
+        },
+      },
+      {
+        actor: {
+          name: {
+            contains: search,
+            mode: "insensitive",
+          },
+        },
+      },
+      {
+        actor: {
+          email: {
+            contains: search,
+            mode: "insensitive",
+          },
+        },
+      },
+      {
+        target: {
+          name: {
+            contains: search,
+            mode: "insensitive",
+          },
+        },
+      },
+      {
+        target: {
+          email: {
+            contains: search,
+            mode: "insensitive",
+          },
+        },
+      },
+    ],
+    ...filters,
+  };
+
+  const [data, count] = await prisma.$transaction([
+    prisma.event.findMany({
+      select: {
+        id: true,
+        occurred_at: true,
+        action: {
+          select: {
+            name: true,
+          },
+        },
+        actor: {
+          select: {
+            name: true,
+          },
+        },
+      },
+      take: pageSize,
+      skip: page * pageSize,
+      where,
+      orderBy: {
+        occurred_at: "desc",
+      },
+    }),
+    prisma.event.count({
+      where,
+    }),
+  ]);
+
+  prisma.action
+    .findFirst({
+      where: {
+        name: "user.searched_activity_log_events",
+      },
+    })
+    .then(async (searchAction) => {
+      if (!searchAction) {
+        throw new Error(
+          "Cannot find `user.searched_activity_log_events` action in DB"
+        );
+      }
+
+      await prisma.event.create({
+        data: {
+          action_id: searchAction.id,
+          location: userLocation,
+          actor_id: userId,
+          metadata: {
+            search,
+            ...filters,
+          },
+        },
+      });
+    })
+    .catch((error) => {
+      console.error("Get Events:", error);
+    });
+
+  return {
+    data,
+    count,
+  };
+}
